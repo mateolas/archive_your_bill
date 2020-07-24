6be55dbeeae52544d75012eadeb17733edad5213
Mateusz Kowara <mateusz.kowara@gmail.com>
2020-07-24 08:05:36 +0200

    Implementing Firebase Authentication

--- archive_your_bill/lib/screens/loginScreen.dart
+++ archive_your_bill/lib/screens/loginScreen.dart
@@ -9,11 +9,14 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
+enum FormType { login, register }
+
 class _LoginScreenState extends State<LoginScreen> {
   final formKey = GlobalKey<FormState>(); //to use form
 
   String _email;
   String _password;
+  FormType _formType = FormType.login;
 
   //validates the date from Form and saves email and password
   bool validateAndSave() {
@@ -21,53 +24,121 @@ class _LoginScreenState extends State<LoginScreen> {
     if (form.validate()) {
       form.save(); //to get values of email and password, calls onSave methods
       return true;
-    } 
+    }
     return false;
   }
 
   //vall this method while pressing the button
-  //inside this method is another method which 
+  //inside this method is another method which
   //validates that password and email aren't empty
-  void validateAndSubmit(){
-    if (validateAndSave()){
-
+  void validateAndSubmit() async {
+    if (validateAndSave()) {
+      try {
+        if (_formType == FormType.login) { //in case we're login in
+          FirebaseUser user = (await FirebaseAuth.instance
+                  .signInWithEmailAndPassword(
+                      email: _email, password: _password))
+              .user;
+          print('Signed in: ${user.uid}');
+        } else { //in case we're registering user
+          FirebaseUser user = (await FirebaseAuth.instance
+                  .createUserWithEmailAndPassword(
+                      email: _email, password: _password))
+              .user;
+          print('Registered user: ${user.uid}');
+        }
+      } catch (e) {
+        print('Error: $e');
+      }
     }
+  }
+
+  void moveToRegister() {
+    //to remove any typed data from previous screen
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin() {
+    //to remove any typed data from previous screen
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-            padding: EdgeInsets.all(16.0),
-            child: Form(
-              key: formKey, //connected with GlobalKey
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  new TextFormField(
-                    decoration: InputDecoration(labelText: 'Email'),
-                    validator: (value) =>
-                        value.isEmpty ? ('Email can\'t be empty') : null,
-                    onSaved: (value) => _email = value,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    validator: (value) =>
-                        value.isEmpty ? ('Password can\'t be empty') : null,
-                         onSaved: (value) => _password = value,
-                  ),
-                  RaisedButton(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    onPressed: validateAndSubmit,
-                  ),
-                ],
-              ),
-            )));
+      appBar: AppBar(),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey, //connected with GlobalKey
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: buildInputs() + buildSubmitButtons(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildInputs() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Email'),
+        validator: (value) => value.isEmpty ? ('Email can\'t be empty') : null,
+        onSaved: (value) => _email = value,
+      ),
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Password'),
+        obscureText: true,
+        validator: (value) =>
+            value.isEmpty ? ('Password can\'t be empty') : null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButtons() {
+    if (_formType == FormType.login) {
+      return [
+        RaisedButton(
+          child: Text(
+            'Login',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          onPressed: validateAndSubmit,
+        ),
+        FlatButton(
+          child: Text(
+            'Create an account',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          onPressed: moveToRegister,
+        )
+      ];
+    } else {
+      return [
+        RaisedButton(
+          child: Text(
+            'Create an account',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          onPressed: validateAndSubmit,
+        ),
+        FlatButton(
+          child: Text(
+            'Have an account ? Login',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          onPressed: moveToLogin,
+        )
+      ];
+    }
   }
 }
 
