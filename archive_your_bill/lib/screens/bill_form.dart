dafbb90169668ea187c4b9a0a9fbd4d288d4eca0
Mateusz Kowara <mateusz.kowara@gmail.com>
2020-08-06 08:20:11 +0200

    Building a "Bill Form" page

--- archive_your_bill/lib/screens/bill_form.dart
+++ archive_your_bill/lib/screens/bill_form.dart
@@ -1,3 +1,4 @@
+import 'package:archive_your_bill/model/bill.dart';
 import 'package:flutter/material.dart';
 
 class BillForm extends StatefulWidget {
@@ -6,13 +7,75 @@ class BillForm extends StatefulWidget {
 }
 
 class _BillFormState extends State<BillForm> {
+  //key to the form
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  List _subingredients = [];
+  //the same as CurrentBill in BillNotifier
+  Bill _currentBill;
+
+  Widget _showImage() {
+    return Text('Image Here');
+  }
+
+  Widget _buildNameField() {
+    return TextFormField(keyboardType: TextInputType.text,
+    style: TextStyle(fontSize:20),
+    validator: (String value) {
+      if(value.isEmpty){
+        return 'Name is required';
+      }
+
+      if(value.length <3 || value.length > 20){
+        return 'Name must be more than 3 and less than 20';
+      }
+
+      return null;
+    },
+    );
+  }
+  
+  Widget _buildCategoryField() {
+    return Text('Image Here');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bill form'),
       ),
-      body: Text("Bill Form"),
+      //to be sure that page will be scrollable
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(32),
+        child: Form(
+          key: _formKey,
+          autovalidate: true,
+          child: Column(
+            children: <Widget>[
+              _showImage(),
+              SizedBox(height: 16),
+              Text(
+                'Create Food',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(height: 16),
+              ButtonTheme(
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Add Image',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                _buildNameField(),
+                _buildCategoryField(),
+                SizedBox(height: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- archive_your_bill/lib/screens/detail.dart
+++ archive_your_bill/lib/screens/detail.dart
@@ -16,50 +16,52 @@ class BillDetail extends StatelessWidget {
           billNotifier.currentBill.name,
         ),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Image.network(billNotifier.currentBill.image),
-              height: 150,
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            Text(
-              billNotifier.currentBill.name,
-              style: TextStyle(
-                fontSize: 40,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Image.network(billNotifier.currentBill.image),
+                height: 150,
               ),
-            ),
-            Text(
-              billNotifier.currentBill.category,
-              style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
-            ),
-            SizedBox(height: 32),
-            GridView.count(
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              padding: EdgeInsets.all(8),
-              crossAxisCount: 3,
-              crossAxisSpacing: 4,
-              mainAxisSpacing: 4,
-              children: billNotifier.currentBill.subIngredients
-                  .map((ingredient) => Card(
-                        color: Colors.black54,
-                        child: Center(
-                          child: Text(
-                            ingredient,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                billNotifier.currentBill.name,
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+              Text(
+                billNotifier.currentBill.category,
+                style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+              ),
+              SizedBox(height: 32),
+              GridView.count(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.all(8),
+                crossAxisCount: 3,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                children: billNotifier.currentBill.subIngredients
+                    .map((ingredient) => Card(
+                          color: Colors.black54,
+                          child: Center(
+                            child: Text(
+                              ingredient,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
                             ),
                           ),
-                        ),
-                      ))
-                  .toList(),
-            ),
-          ],
+                        ))
+                    .toList(),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
