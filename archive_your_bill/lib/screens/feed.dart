0a010117bafafef83ffdcbb54194943ae444bb49
Mateusz Kowara <mateusz.kowara@gmail.com>
2020-08-12 22:58:44 +0200

    Removed bug 'Null text in String'. Caused by Display name and Pictures.

--- archive_your_bill/lib/screens/feed.dart
+++ archive_your_bill/lib/screens/feed.dart
@@ -12,9 +12,12 @@ class Feed extends StatefulWidget {
 }
 
 class _FeedState extends State<Feed> {
+
+  
   @override
   void initState() {
-    FoodNotifier foodNotifier = Provider.of<FoodNotifier>(context, listen: false);
+    FoodNotifier foodNotifier =
+        Provider.of<FoodNotifier>(context, listen: false);
     getFoods(foodNotifier);
     super.initState();
   }
@@ -29,13 +32,14 @@ class _FeedState extends State<Feed> {
     }
 
     print("building Feed");
+    print('First authnotifier ${authNotifier.user.displayName}');
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          authNotifier.user != null ? authNotifier.user.displayName : "Main page",
-        ),
+        //added display.name
+        title: Text(authNotifier.user.displayName != null ?  authNotifier.user.displayName : "Main page" ),
         actions: <Widget>[
-          // action button
+          // action button - logout
           FlatButton(
             onPressed: () => signout(authNotifier),
             child: Text(
@@ -49,18 +53,20 @@ class _FeedState extends State<Feed> {
         child: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              leading: Image.network(
-                foodNotifier.foodList[index].image != null
-                    ? foodNotifier.foodList[index].image
-                    : 'https://www.testingxperts.com/wp-content/uploads/2019/02/placeholder-img.jpg',
-                width: 120,
-                fit: BoxFit.fitWidth,
-              ),
+              leading: Text('Test'),
+              //  Image.network(
+              //   foodNotifier.foodList[index].image != null
+              //       ? foodNotifier.foodList[index].image
+              //       : 'https://www.testingxperts.com/wp-content/uploads/2019/02/placeholder-img.jpg',
+              //   width: 120,
+              //   fit: BoxFit.fitWidth,
+              // ),
               title: Text(foodNotifier.foodList[index].name),
               subtitle: Text(foodNotifier.foodList[index].category),
               onTap: () {
                 foodNotifier.currentFood = foodNotifier.foodList[index];
-                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
                   return FoodDetail();
                 }));
               },
@@ -89,6 +95,8 @@ class _FeedState extends State<Feed> {
         child: Icon(Icons.add),
         foregroundColor: Colors.white,
       ),
+      
     );
+    
   }
 }
