0798c0ad766e189fc1c3ed188c21540344d92ab9
Mateusz Kowara <mateusz.kowara@gmail.com>
2020-08-22 23:17:00 +0200

    Implementing StreamBuilder part 2

--- archive_your_bill/lib/screens/feed.dart
+++ archive_your_bill/lib/screens/feed.dart
@@ -34,12 +34,17 @@ class _FeedState extends State<Feed> {
     }
 
     Stream<QuerySnapshot> getBillsStreamSnapshots(BuildContext context) async* {
-      final uid = await Provider.of(context).auth.getCurrentUID();
+      final FirebaseAuth auth = FirebaseAuth.instance;
+      final FirebaseUser user = await auth.currentUser();
+      final uid = user.uid;
+      
+    
       yield* Firestore.instance
           .collection('userData')
-          .document(uid)
+          .document(uid.toString())
           .collection('bills')
           .snapshots();
+      print('Uid: $uid');
     }
 
     print("building Feed");
@@ -66,38 +71,43 @@ class _FeedState extends State<Feed> {
         child: StreamBuilder(
             stream: getBillsStreamSnapshots(context),
             builder: (context, snapshot) {
-              if (!snapshot.hasData) {return const Text("Loading...");} else {
-              return ListView.separated(
-                itemCount: snapshot.data.documents.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: Text('Test'),
-                    //  Image.network(
-                    //   foodNotifier.foodList[index].image != null
-                    //       ? foodNotifier.foodList[index].image
-                    //       : 'https://www.testingxperts.com/wp-content/uploads/2019/02/placeholder-img.jpg',
-                    //   width: 120,
-                    //   fit: BoxFit.fitWidth,
-                    // ),
-                    title: Text(snapshot.data.documents[index].name),
-                    subtitle: Text(snapshot.data.documents[index].category),
-                    onTap: () {
-                      foodNotifier.currentFood = snapshot.data.documents[index];
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (BuildContext context) {
-                        return FoodDetail();
-                      }));
-                    },
-                  );
-                },
-              
-                separatorBuilder: (BuildContext context, int index) {
-                  return Divider(
-                    color: Colors.black,
-                  );
-                },
-              );
-            }}),
+              if (!snapshot.hasData) {
+                return const Text("Loading...");
+              } else {
+                return ListView.separated(
+                  itemCount: foodNotifier.foodList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: Text('Test'),
+                      //  Image.network(
+                      //   foodNotifier.foodList[index].image != null
+                      //       ? foodNotifier.foodList[index].image
+                      //       : 'https://www.testingxperts.com/wp-content/uploads/2019/02/placeholder-img.jpg',
+                      //   width: 120,
+                      //   fit: BoxFit.fitWidth,
+                      // ),
+                      //title: Text(snapshot.data.documents[index].name),
+                      //subtitle: Text(snapshot.data.documents[index].category),
+                      title: Text(foodNotifier.foodList[index].name),
+                      subtitle: Text(foodNotifier.foodList[index].category),
+                      onTap: () {
+                        foodNotifier.currentFood =
+                            foodNotifier.foodList[index];
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return FoodDetail();
+                        }));
+                      },
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Divider(
+                      color: Colors.black,
+                    );
+                  },
+                );
+              }
+            }),
         onRefresh: _refreshList,
       ),
       floatingActionButton: FloatingActionButton(
