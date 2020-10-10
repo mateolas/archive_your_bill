9f3e5aa8cfadb0e8492c6d174a00fc59c39611c9
Mateusz Kowara <mateusz.kowara@gmail.com>
2020-10-10 20:58:32 +0200

    Updating View of the Login Page

--- archive_your_bill/lib/screens/login.dart
+++ archive_your_bill/lib/screens/login.dart
@@ -3,6 +3,7 @@ import 'package:archive_your_bill/model/user.dart';
 import 'package:archive_your_bill/notifier/auth_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 enum AuthMode { Signup, Login }
 
@@ -22,7 +23,8 @@ class _LoginState extends State<Login> {
 
   @override
   void initState() {
-    AuthNotifier authNotifier = Provider.of<AuthNotifier>(context, listen: false);
+    AuthNotifier authNotifier =
+        Provider.of<AuthNotifier>(context, listen: false);
     initializeCurrentUser(authNotifier);
     super.initState();
   }
@@ -34,7 +36,8 @@ class _LoginState extends State<Login> {
 
     _formKey.currentState.save();
 
-    AuthNotifier authNotifier = Provider.of<AuthNotifier>(context, listen: false);
+    AuthNotifier authNotifier =
+        Provider.of<AuthNotifier>(context, listen: false);
 
     if (_authMode == AuthMode.Login) {
       login(_user, authNotifier);
@@ -47,9 +50,9 @@ class _LoginState extends State<Login> {
     return TextFormField(
       decoration: InputDecoration(
         labelText: "Display Name",
-        labelStyle: TextStyle(color: Colors.white54),
+        labelStyle: TextStyle(color: Colors.white),
       ),
-      keyboardType: TextInputType.text,
+      keyboardType: TextInputType.emailAddress,
       style: TextStyle(fontSize: 26, color: Colors.white),
       cursorColor: Colors.white,
       validator: (String value) {
@@ -69,32 +72,80 @@ class _LoginState extends State<Login> {
     );
   }
 
-  Widget _buildEmailField() {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: "Email",
-        labelStyle: TextStyle(color: Colors.white54),
+  final kBoxDecorationStyle = BoxDecoration(
+    color: Color(0xFFFFCC80),
+    borderRadius: BorderRadius.circular(10.0),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black12,
+        blurRadius: 6.0,
+        offset: Offset(0, 2),
       ),
-      keyboardType: TextInputType.emailAddress,
-      initialValue: 'test@test.com',
-      style: TextStyle(fontSize: 26, color: Colors.white),
-      cursorColor: Colors.white,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Email is required';
-        }
+    ],
+  );
 
-        if (!RegExp(
-                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-            .hasMatch(value)) {
-          return 'Please enter a valid email address';
-        }
+  final kHintTextStyle = TextStyle(
+    color: Colors.white54,
+    fontFamily: 'OpenSans',
+    fontSize: 20,
+  );
 
-        return null;
-      },
-      onSaved: (String value) {
-        _user.email = value;
-      },
+  final kLabelStyle = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'OpenSans',
+    fontSize: 20,
+  );
+
+  Widget _buildEmailField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Email',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Email',
+              hintStyle: kHintTextStyle,
+            ),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Email is required';
+              }
+
+              if (!RegExp(
+                      r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                  .hasMatch(value)) {
+                return 'Please enter a valid email address';
+              }
+
+              return null;
+            },
+            onSaved: (String value) {
+              _user.email = value;
+            },
+          ),
+        ),
+      ],
     );
   }
 
@@ -102,10 +153,10 @@ class _LoginState extends State<Login> {
     return TextFormField(
       decoration: InputDecoration(
         labelText: "Password",
-        labelStyle: TextStyle(color: Colors.white54),
+        labelStyle: TextStyle(color: Colors.black),
       ),
-      style: TextStyle(fontSize: 26, color: Colors.white),
-      cursorColor: Colors.white,
+      style: TextStyle(fontSize: 26, color: Colors.black),
+      cursorColor: Colors.black,
       obscureText: true,
       controller: _passwordController,
       validator: (String value) {
@@ -129,10 +180,10 @@ class _LoginState extends State<Login> {
     return TextFormField(
       decoration: InputDecoration(
         labelText: "Confirm Password",
-        labelStyle: TextStyle(color: Colors.white54),
+        labelStyle: TextStyle(color: Colors.black),
       ),
-      style: TextStyle(fontSize: 26, color: Colors.white),
-      cursorColor: Colors.white,
+      style: TextStyle(fontSize: 26, color: Colors.black),
+      cursorColor: Colors.black,
       obscureText: true,
       validator: (String value) {
         if (_passwordController.text != value) {
@@ -149,62 +200,102 @@ class _LoginState extends State<Login> {
     print("Building login screen");
 
     return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.expand(
-          height: MediaQuery.of(context).size.height,
-        ),
-        decoration: BoxDecoration(color: Color(0xff34056D)),
-        child: Form(
-          autovalidate: true,
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(32, 96, 32, 0),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "Please Sign In",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 36, color: Colors.white),
-                  ),
-                  SizedBox(height: 32),
-                  _authMode == AuthMode.Signup ? _buildDisplayNameField() : Container(),
-                  _buildEmailField(),
-                  _buildPasswordField(),
-                  _authMode == AuthMode.Signup ? _buildConfirmPasswordField() : Container(),
-                  SizedBox(height: 32),
-                  ButtonTheme(
-                    minWidth: 200,
-                    child: RaisedButton(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        'Switch to ${_authMode == AuthMode.Login ? 'Signup' : 'Login'}',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _authMode =
-                              _authMode == AuthMode.Login ? AuthMode.Signup : AuthMode.Login;
-                        });
-                      },
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  ButtonTheme(
-                    minWidth: 200,
-                    child: RaisedButton(
-                      padding: EdgeInsets.all(10.0),
-                      onPressed: () => _submitForm(),
-                      child: Text(
-                        _authMode == AuthMode.Login ? 'Login' : 'Signup',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFFFFCC80),
+                    Color(0xFFFFB74D),
+                    Color(0xFFFFA726),
+                    Color(0xFFFF9800),
+                  ],
+                  stops: [0.1, 0.4, 0.7, 0.9],
+                ),
               ),
             ),
-          ),
+            Container(
+              constraints: BoxConstraints.expand(
+                height: MediaQuery.of(context).size.height,
+              ),
+              //decoration: BoxDecoration(color: Colors.white),
+              child: Form(
+                autovalidate: false,
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(32, 96, 32, 0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Please Sign In",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 36, color: Colors.white),
+                        ),
+                        SizedBox(height: 32),
+                        _authMode == AuthMode.Signup
+                            ? _buildDisplayNameField()
+                            : Container(),
+                        _buildEmailField(),
+                        _buildPasswordField(),
+                        _authMode == AuthMode.Signup
+                            ? _buildConfirmPasswordField()
+                            : Container(),
+                        SizedBox(height: 32),
+                        ButtonTheme(
+                          minWidth: 200,
+                          child: RaisedButton(
+                            padding: EdgeInsets.all(10.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.orange),
+                            ),
+                            child: Text(
+                              'Switch to ${_authMode == AuthMode.Login ? 'Signup' : 'Login'}',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _authMode = _authMode == AuthMode.Login
+                                    ? AuthMode.Signup
+                                    : AuthMode.Login;
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        ButtonTheme(
+                          minWidth: 200,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.orange),
+                          ),
+                          child: RaisedButton(
+                            padding: EdgeInsets.all(10.0),
+                            onPressed: () => _submitForm(),
+                            child: Text(
+                              _authMode == AuthMode.Login ? 'Login' : 'Signup',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
